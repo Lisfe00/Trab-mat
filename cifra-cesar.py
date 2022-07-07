66707dad368529feac043596f0f005745a0fd86e
Lisfe00 <78762644+Lisfe00@users.noreply.github.com>
2022-07-07 20:08:53 -0300

    atualização

--- cifra-cesar.py
+++ cifra-cesar.py
@@ -1,26 +1,3 @@
 MODE_ENCRYPT = 1
 MODE_DECRYPT = 0
 
-def caesar(data, key, mode):
-    alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÔÕÍÚÇ '
-    new_data = ''
-    for c in data:
-        index = alphabet.find(c)
-        if index == -1:
-            new_data += c
-        else:
-            new_index = index + key if mode == MODE_ENCRYPT else index - key
-            new_index = new_index % len(alphabet)
-            new_data += alphabet[new_index:new_index+1]
-    return new_data
-
-# Tests
-key = 5
-original = 'hello world'
-print('\n   Original:', original)
-ciphered = caesar(original, key, MODE_ENCRYPT)
-print('\n Encriptada:', ciphered)
-plain = caesar(ciphered, key, MODE_DECRYPT)
-print('\n Decriptada:', plain)
-print('\n chave usada:', key)
-print('\n')
